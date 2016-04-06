9cbb290223a892c645e53f0512949273c0e73638
johnlindquist <johnlindquist@gmail.com>
2016-04-06 09:20:45 -0600

    updating config

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ SystemJS.config({
         "emitDecoratorMetadata": true
     },
     map: {
-        "rxjs": "node_modules/rxjs@5.0.0-beta.2",
+        "rxjs": "node_modules/rxjs",
         "angular2": "node_modules/angular2",
         "@ngrx": "node_modules/@ngrx",
         "typescript": "node_modules/typescript/lib/typescript.js"
